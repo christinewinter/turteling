88dc7637791bcb9ceafeefd9e0db4bd3d61acf73
Christine Winter <ChristineWinter1@gmx.de>
2020-11-21 14:08:13 +0100

    add files to draw koch snowflake

---
+++ snowflake_turtle.py
@@ -0,0 +1,47 @@
+from turtle import Turtle, Screen
+
+
+def snowflake(length_side, levels):
+    if levels == 0:
+        t.forward(length_side)
+        return
+    length_side = length_side / 3
+    snowflake(length_side, levels - 1)
+    t.left(60)
+    snowflake(length_side, levels - 1)
+    t.right(120)
+    snowflake(length_side, levels - 1)
+    t.left(60)
+    snowflake(length_side, levels - 1)
+
+
+def draw_one_flake(length, level):
+    t.pendown()
+    for i in range(3):
+        snowflake(length, level)
+        t.right(120)
+    t.penup()
+
+
+t = Turtle()
+screen = Screen()
+screen.bgcolor("#3659d9")
+
+# defining the speed of the turtle#
+t.speed(99999)
+length = 100.0
+level = 2
+t.pensize(8)
+t.hideturtle()
+
+
+for i in range(6):
+    t.pencolor("White")
+    draw_one_flake(length, level)
+    t.left(120)
+    t.forward(length/3)
+    t.right(60)
+    t.forward(length/3)
+
+
+screen.exitonclick()
